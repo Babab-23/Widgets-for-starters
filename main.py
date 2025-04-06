54c79477208658d72464921194fc7d2447121484
Babab-23 <adrianidialu@gmail.com>
2025-04-06 20:22:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Import necessary libraries 
+from tkinter import *
+#Create Window
+window = Tk()
+#Set the window title and geometry
+window.title("Demo Window")
+window.geometry("400x300")
+window.mainloop()
